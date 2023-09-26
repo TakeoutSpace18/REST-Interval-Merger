f713f7d907ec84b8dfdcd5abf23c77a62aca4f0f
Pavel Urdin <p.urdin@g.nsu.ru>
2023-09-26 21:53:22 +0700

    Test refactoring

---
+++ src/test/java/com/github/takeoutspace18/RestIntervalMerger/IntervalsControllerIntegrationTests.java
@@ -0,0 +1,65 @@
+package com.github.takeoutspace18.RestIntervalMerger;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class IntervalsControllerIntegrationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Test
+	public void minDigitIntervalIsReturned() throws Exception {
+		String mockJsonIntervals = "[[1, 4], [2, 6], [8, 10], [10, 11]]";
+		String expectedResult = "[1, 6]";
+		String kind = "digits";
+
+		postMergeMockIntervals(kind, mockJsonIntervals);
+		checkGetMinInterval(kind, expectedResult);
+	}
+	@Test
+	public void minLetterIntervalIsReturned() throws Exception {
+		String mockJsonIntervals = "[[\"a\", \"c\"], [\"b\", \"d\"], [\"e\", \"g\"], [\"h\", \"i\"]]";
+		String expectedResult = "[\"a\", \"d\"]";
+		String kind = "letters";
+
+		postMergeMockIntervals(kind, mockJsonIntervals);
+		checkGetMinInterval(kind, expectedResult);
+	}
+
+	@Test
+	public void minIntervalIsReturnedWithDoubleMerge() throws Exception {
+		String kind = "digits";
+		postMergeMockIntervals(kind, "[[33, 56]]");
+		checkGetMinInterval(kind, "[33, 56]");
+		postMergeMockIntervals(kind, "[[33, 40]]");
+		checkGetMinInterval(kind, "[33, 40]");
+	}
+
+	@Test
+	public void notFoundIsReturned() throws Exception {
+		this.mockMvc.perform(get("/api/nv1/intervals/min?kind=digits")).andExpect(status().isNotFound());
+	}
+
+	private void postMergeMockIntervals(String kind, String mockJsonIntervals) throws Exception {
+		this.mockMvc.perform(post("/api/v1/intervals/merge?kind={kind}", kind).contentType(MediaType.APPLICATION_JSON).content(mockJsonIntervals))
+				.andDo(print()).andExpect(status().isOk());
+	}
+
+	private void checkGetMinInterval(String kind, String expectedJsonResult) throws Exception {
+		this.mockMvc.perform(get("/api/v1/intervals/min?kind={kind}", kind)).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(expectedJsonResult));
+	}
+}
